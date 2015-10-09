18234e7099c6d391d05b4d8295dad90172089288
Marcelo Pietragalla <mpietragalla@gmail.com>
2015-10-09 17:31:32 -0300

    alterações de material e propriedades dos players

--- Assets/scripts/freeClasses/GameGlobals.cs
+++ Assets/scripts/freeClasses/GameGlobals.cs
@@ -14,12 +14,12 @@ public class GameGlobals  {
 	
 
 	static public PlayersData[] playersToLoad = new PlayersData[]{
-		new PlayersData(GameGlobals.atrib.PLAYER_ONE, new Vector4(1f,0f,0f,1f), 2.0f, 1.0f, 10f, 0.5f, "bola3", "roedorPath", "power"),
-		new PlayersData(GameGlobals.atrib.PLAYER_TWO, new Vector4(0f,1f,0f,1f), 1.0f, 2.0f, 10f, 0.8f, "bola2", "roedorPath", "power"),
-		new PlayersData(GameGlobals.atrib.NPC, new Vector4(0f,0f,1f,1f), 2.0f, 2.0f, 10f, 0.1f, "bola1", "roedorPath", "power"),
-		new PlayersData(GameGlobals.atrib.NPC, new Vector4(0f,1f,1f,1f), 0.9f, 0.5f, 07f, 0.3f, "bola2", "roedorPath", "power"),
-		new PlayersData(GameGlobals.atrib.NPC, new Vector4(1f,0f,1f,1f), 4.0f, 3.0f, 50f, 0.8f, "bola3", "roedorPath", "power"),
-		new PlayersData(GameGlobals.atrib.NPC, new Vector4(1f,1f,0f,1f), 1.0f, 0.9f, 10f, 0.2f, "bola2", "roedorPath", "power")
+		new PlayersData(GameGlobals.atrib.PLAYER_ONE, new Vector4(1f,0f,0f,0.05f), 2.0f, 1.0f, 10f, 0.5f, "bola4", "roedorPath", "power"),
+		new PlayersData(GameGlobals.atrib.PLAYER_TWO, new Vector4(0.14f,0.19f,0.23f,1f), 4.0f, 3.0f, 50f, 0.8f, "bola2", "roedorPath", "power"),
+		new PlayersData(GameGlobals.atrib.NPC, new Vector4(0f,0f,1f,1f), 3.0f, 2.0f, 10f, 0.1f, "bola1", "roedorPath", "power"),
+		new PlayersData(GameGlobals.atrib.NPC, new Vector4(0f,1f,1f,1f), 3.5f, 0.5f, 07f, 0.3f, "bola1", "roedorPath", "power"),
+		new PlayersData(GameGlobals.atrib.NPC, new Vector4(0.5f,0.35f,0.1f,1f), 3.5f, 0.2f, 20f, 0.8f, "bola3", "roedorPath", "power"),
+		new PlayersData(GameGlobals.atrib.NPC, new Vector4(1f,1f,0f,1f), 2.7f, 0.9f, 10f, 0.2f, "bola2", "roedorPath", "power")
 	};
 
 
