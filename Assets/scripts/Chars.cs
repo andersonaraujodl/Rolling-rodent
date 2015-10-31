f79454d846d3d3b62204d89c7f0da586f786d67c
Diego <digblack@yahoo.com.br>
2015-10-31 16:36:07 -0200

    Menu seleção de personagens e ajustes

---
+++ Assets/scripts/Chars.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chars{
+
+    static public Rodents[] rodentsGame = new Rodents[]
+    {
+        new Rodents("Ginbal", "hamster", 2f, 7.0f, "Hamster ágil e esperto"),
+        new Rodents("Ninky", "ratazana", 3f, 7.0f, "Hamster ágil e esperto")
+    };
+
+    static public Balls[] ballsGame = new Balls[]
+    {
+        new Balls("Plastico", "bola1", new Vector4(1f,1f,1f,1f), 1.0f, 0.5f, "power", "Bola de plástico comum"),
+        new Balls("Aço", "bola2", new Vector4(0f,0f,1f,1f), 2.0f, 1.0f, "power", "Bola de aço forte e resistente"),
+        new Balls("Madeira", "bola3", new Vector4(0.5f,0.7f,0.2f,0.9f), 1.5f, 0.7f, "power", "Bola de madeira reciclada"),
+        new Balls("Ar", "bola4", new Vector4(0.14f,0.19f,0.23f,1f), 0.5f, 0.2f, "power", "Bolha de ar leve como o vento")
+    };
+
+}
---
+++ Assets/selection_screen/Selecao.cs
@@ -0,0 +1,292 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Selecao : MonoBehaviour
+{
+
+    public GameObject[] bola;
+    public GameObject[] roedor;
+    public GameObject[] bolaCena;
+    public GameObject[] roedorCena;
+    public GameObject personagemJogo;
+
+    public GameObject[] playerPos;
+
+    static public int[] nivel;
+    public int indexNivel;
+
+    private Rigidbody rb;
+
+    public Text[] txtTela;
+    static public string player1_cpu, player2_cpu, player3_cpu, player4_cpu, player5_cpu, player6_cpu;
+
+    // Use this for initialization
+    void Start()
+    {
+       Object.DontDestroyOnLoad(this);
+
+        indexNivel = 0;
+        nivel = new int[18];
+        bolaCena = new GameObject[6];
+        roedorCena = new GameObject[6];
+
+        //player1Pos = GameObject.Find("p1");
+        //player2Pos = GameObject.Find("p2");
+
+        player1_cpu = "Player 1";
+        player2_cpu = "CPU";
+        player3_cpu = "CPU";
+        player4_cpu = "CPU";
+        player5_cpu = "CPU";
+        player6_cpu = "CPU";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if (Application.loadedLevelName == "jogo" )
+        {
+            personagemJogo = GameObject.Find("Player");
+            bolaCena = (GameObject)Instantiate(bola[nivel[0]], personagemJogo.transform.position, personagemJogo.transform.rotation);
+            Destroy(personagemJogo);
+            
+            bolaCena.AddComponent(System.Type.GetType("Move"));
+            Destroy(this);
+        }*/
+
+        if (Application.loadedLevelName == "selecao")
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+
+                Application.LoadLevel("test_arena");
+
+
+
+
+            }
+
+            int k = 1;
+
+            for(int i = 0; i< roedorCena.Length; i++)
+            {
+                if (!roedorCena[i])
+                {
+                    roedorCena[i] = (GameObject)Instantiate(roedor[nivel[k]], playerPos[i].transform.position, Quaternion.identity);
+                    rb = roedorCena[i].GetComponent<Rigidbody>();
+                    rb.useGravity = false;
+                }
+                else if (!bolaCena[i])
+                {
+                    bolaCena[i] = (GameObject)Instantiate(bola[nivel[k+1]], playerPos[i].transform.position, Quaternion.identity);
+                    rb = bolaCena[i].GetComponent<Rigidbody>();
+                    rb.useGravity = false;
+                }
+                else
+                {
+                    roedorCena[i].transform.Rotate(0, 90 * Time.deltaTime, 0);
+                    bolaCena[i].transform.Rotate(0, 90 * Time.deltaTime, 0);
+                }
+                txtTela[k].text = Chars.rodentsGame[nivel[k]].nome;
+                txtTela[k+1].text = Chars.ballsGame[nivel[k+1]].nome;
+                k = k + 3;
+            }
+
+            /*if (!roedorCena[0])
+            {
+                roedorCena[0] = (GameObject)Instantiate(roedor[nivel[1]], player1Pos.transform.position, Quaternion.identity);
+                rb2 = roedorCena[0].GetComponent<Rigidbody>();
+                rb2.useGravity = false;
+            }
+            else if (!bolaCena[0])
+            {
+                bolaCena[0] = (GameObject)Instantiate(bola[nivel[2]], player1Pos.transform.position, Quaternion.identity);
+                rb1 = bolaCena[0].GetComponent<Rigidbody>();
+                rb1.useGravity = false;
+            }
+            if (!roedorCena[1])
+            {
+                roedorCena[1] = (GameObject)Instantiate(roedor[nivel[4]], player2Pos.transform.position, Quaternion.identity);
+                rb2 = roedorCena[1].GetComponent<Rigidbody>();
+                rb2.useGravity = false;
+            }
+            else if (!bolaCena[1])
+            {
+                bolaCena[1] = (GameObject)Instantiate(bola[nivel[5]], player2Pos.transform.position, Quaternion.identity);
+                rb1 = bolaCena[1].GetComponent<Rigidbody>();
+                rb1.useGravity = false;
+            }
+            else
+            {
+                roedorCena[0].transform.Rotate(0, 90 * Time.deltaTime, 0);
+                bolaCena[0].transform.Rotate(0, 90 * Time.deltaTime, 0);
+            }*/
+
+            txtTela[0].text = player1_cpu;
+            txtTela[3].text = player2_cpu;
+            txtTela[6].text = player3_cpu;
+            txtTela[9].text = player4_cpu;
+            txtTela[12].text = player5_cpu;
+            txtTela[15].text = player6_cpu;
+            //txtTela[1].text = Chars.rodentsGame[nivel[1]].nome;
+            //txtTela[2].text = Chars.ballsGame[nivel[2]].nome;
+
+            corTexto(indexNivel);
+
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (indexNivel == nivel.Length - 1) indexNivel = 0;
+                else indexNivel++;
+
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (indexNivel == 0) indexNivel = nivel.Length - 1;
+                else indexNivel--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (indexNivel % 3 == 1)
+                {
+                    if (nivel[indexNivel] == 0) nivel[indexNivel] = roedor.Length - 1;
+                    else nivel[indexNivel]--;
+                }
+                else if (indexNivel % 3 == 2)
+                {
+                    if (nivel[indexNivel] == 0) nivel[indexNivel] = bola.Length - 1;
+                    else nivel[indexNivel]--;
+                }
+                else
+                {
+                    playerCPU(indexNivel);
+                }
+
+                Destroy_Obj(indexNivel);
+
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (indexNivel % 3 == 1)
+                {
+                    if (nivel[indexNivel] == roedor.Length - 1) nivel[indexNivel] = 0;
+                    else nivel[indexNivel]++;
+                }
+                else if (indexNivel % 3 == 2)
+                {
+                    if (nivel[indexNivel] == bola.Length - 1) nivel[indexNivel] = 0;
+                    else nivel[indexNivel]++;
+                }
+                else
+                {
+                    playerCPU(indexNivel);
+                }
+                Destroy_Obj(indexNivel);
+            }
+
+            
+        }
+        else
+        {
+            
+
+        }
+    }
+
+    void Destroy_Obj(int obj)
+    {
+        switch (obj)
+        {
+            case 1:
+                Destroy(roedorCena[0]);
+                break;
+            case 2:
+                Destroy(bolaCena[0]);
+                break;
+            case 4:
+                Destroy(roedorCena[1]);
+                break;
+            case 5:
+                Destroy(bolaCena[1]);
+                break;
+            case 7:
+                Destroy(roedorCena[2]);
+                break;
+            case 8:
+                Destroy(bolaCena[2]);
+                break;
+            case 10:
+                Destroy(roedorCena[3]);
+                break;
+            case 11:
+                Destroy(bolaCena[3]);
+                break;
+            case 13:
+                Destroy(roedorCena[4]);
+                break;
+            case 14:
+                Destroy(bolaCena[4]);
+                break;
+            case 16:
+                Destroy(roedorCena[5]);
+                break;
+            case 17:
+                Destroy(bolaCena[5]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void playerCPU(int nivel)
+    {
+        if (nivel == 0)
+        {
+            if (player1_cpu == "Player 1") player1_cpu = "CPU";
+            else if (player1_cpu == "CPU") player1_cpu = "Player 1";
+        }
+        if (nivel == 3)
+        {
+            if (player2_cpu == "Player 2") player2_cpu = "CPU";
+            else if (player2_cpu == "CPU") player2_cpu = "Player 2";
+        }
+        if (nivel == 6)
+        {
+            if (player3_cpu == "Player 3") player3_cpu = "CPU";
+            else if (player3_cpu == "CPU") player3_cpu = "Player 3";
+        }
+        if (nivel == 9)
+        {
+            if (player4_cpu == "Player 4") player4_cpu = "CPU";
+            else if (player4_cpu == "CPU") player4_cpu = "Player 4";
+        }
+        if (nivel == 12)
+        {
+            if (player5_cpu == "Player 5") player5_cpu = "CPU";
+            else if (player5_cpu == "CPU") player5_cpu = "Player 5";
+        }
+        if (nivel == 15)
+        {
+            if (player6_cpu == "Player 6") player6_cpu = "CPU";
+            else if (player6_cpu == "CPU") player6_cpu = "Player 6";
+        }
+        
+    }
+
+    private void corTexto(int nivel)
+    {
+        for(int j = 0; j < txtTela.Length; j++)
+        {
+            txtTela[j].color = Color.black;
+        }
+        txtTela[nivel].color = Color.red;
+    }
+       
+
+}
